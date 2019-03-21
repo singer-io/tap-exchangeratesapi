a9a3cbb2b20f957cfbc8e14170a1e07bebb7c9cd
Tim Visher <tim@stitchdata.com>
2019-03-21 19:02:23 +0000

    Restructure code

--- setup.py
+++ setup.py
@@ -6,14 +6,21 @@ setup(name='tap-exchangeratesapi',
       version='0.1.0',
       description='Singer.io tap for extracting currency exchange rate data from the exchangeratesapi.io API',
       author='Stitch',
-      url='http://singer.io',
+      url='http://github.com/singer-io/tap-exchangeratesapi',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_exchangeratesapi'],
       install_requires=['singer-python==5.3.3',
                         'backoff==1.3.2',
                         'requests==2.21.0'],
+      extras_requir={
+          'dev': [
+              'ipdb==0.11'
+          ]
+      },
       entry_points='''
           [console_scripts]
           tap-exchangeratesapi=tap_exchangeratesapi:main
       ''',
+      packages=['tap_exchangeratesapi'],
+      include_package_data=True
 )
