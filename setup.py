af7386fb87b0669a600d852c2327d6af26a6c506
Gord Nuttall <gord.nuttall@adswerve.com>
2023-04-12 21:24:41 -0600

    setup.py fixes

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
-#!/usr/bin/env python
-
 from setuptools import setup, find_packages
 
 setup(name='tap-exchangeratesapi',
       version='1.0.1',
-      description='Singer.io tap for extracting currency exchange rate data from the exchangeratesapi.io API by Adswerve',
-      author='Adswerve',
+      description='Singer.io tap for extracting currency exchange rate data from the exchangeratesapi.io API',
+      author='Stitch',
       url='http://github.com/adswerve/tap-exchangeratesapi',
-      download_url='https://github.com/adswerve/tap-exchangeratesapi/archive/refs/tags/1.0.1.zip',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_exchangeratesapi'],
       install_requires=['singer-python==5.13.0',
                         'backoff==1.8.0',
                         'requests==2.28.2'],
+      extras_require={
+          'dev': [
+              'ipdb==0.11'
+          ]
+      },
       entry_points='''
           [console_scripts]
           tap-exchangeratesapi=tap_exchangeratesapi:main
-      '''
+      ''',
+      packages=['tap_exchangeratesapi'],
+      include_package_data=True
 )
